6a8f79ef61d401bcc9f77adf7c5510b696585d91
Toni Asikainen <toniasikainen@outlook.com>
2021-11-13 10:22:29 +0200

    osa 4.2 ohjelma jaoteltu osiin

---
+++ app.js
@@ -0,0 +1,29 @@
+const config = require('./utils/config')
+const express = require('express')
+app = express()
+const cors = require('cors')
+const blogsRouter = require('./controllers/blogs')
+const middleware = require('./utils/middleware')
+const mongoose = require('mongoose')
+const logger = require('./utils/logger')
+
+
+mongoose.connect(config.MONGODB_URI)
+  .then(() => {
+    logger.info('connected to MongoDB')
+  })
+  .catch((error) => {
+    logger.error('error connection to MongoDB:', error.message)
+  })
+
+app.use(cors())
+app.use(express.static('build'))
+app.use(express.json())
+app.use(middleware.requestLogger)
+
+app.use('/api/blogs',blogsRouter)
+
+app.use(middleware.unknownEndPoint)
+app.use(middleware.errorHandler)
+
+module.exports = app
