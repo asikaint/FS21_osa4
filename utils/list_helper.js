f313cf66223c70546cd501b3095d661fdb01ea5d
Toni Asikainen <toniasikainen@outlook.com>
2022-01-29 16:01:12 +0200

    tehtävä 4.8

--- utils/list_helper.js
+++ utils/list_helper.js
@@ -2,6 +2,21 @@ const dummy = (blogs) => {
     return 1
 }
   
+const favoriteBlog = (blogs) => {
+
+    const blogLiked = blogs.find(x => x.likes === Math.max(...blogs.map(blog=>blog.likes))) 
+    const { title,author, likes } = blogLiked
+    // console.log(`typeof author`, typeof author)
+    const returnBlog = [
+        {
+            author,
+            title, 
+            likes: Number(likes)
+        }
+    ]
+    return returnBlog
+}
+
 const totalLikes = (blogs) => {
     const reducer = (likes,blogs) => likes+blogs.likes
     return blogs.reduce(reducer, 0)
@@ -9,5 +24,6 @@ const totalLikes = (blogs) => {
 
 module.exports = {
     dummy,
-    totalLikes
+    totalLikes,
+    favoriteBlog
 }
--- utils/logger.js
+++ utils/logger.js
@@ -1,9 +1,14 @@
 const error = (...params) => {
-  console.error(params)
+  if (process.env.NODE_ENV !== 'test') {
+    console.error(params)
+  }
 }
 
 const info = (...params) => {
-  console.log(params)
+  if (process.env.NODE_ENV !== 'test') {
+    console.log(params)
+  }
+  
 }
 
 module.exports = {
