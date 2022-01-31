db8d59c3927717dfc52ecf21c109e9136876b2b0
Toni Asikainen <toniasikainen@outlook.com>
2022-01-31 22:46:08 +0200

    lisätty test_helper.js

--- tests/blog_api.test.js
+++ tests/blog_api.test.js
@@ -2,46 +2,28 @@ const { json } = require('express')
 const mongoose = require('mongoose')
 const supertest = require('supertest')
 const app = require('../app')
-
+const helper = require('./test_helper')
 const api = supertest(app)
 
 const Blog = require('../models/blog')
-const initialBlogs = [
-  {
-    _id: "5a422a851b54a676234d17f7",
-    title: "React patterns",
-    author: "Michael Chan",
-    url: "https://reactpatterns.com/",
-    likes: 7,
-    __v: 0
-  },
-  {
-    _id: "5a422aa71b54a676234d17f8",
-    title: "Go To Statement Considered Harmful",
-    author: "Edsger W. Dijkstra",
-    url: "http://www.u.arizona.edu/~rubinson/copyright_violations/Go_To_Considered_Harmful.html",
-    likes: 5,
-    __v: 0
-  }
-]
+
 beforeEach(async () => {
   await Blog.deleteMany({})
-  let blogObject = new Blog(initialBlogs[0])
+  let blogObject = new Blog(helper.initialBlogs[0])
   await blogObject.save()
-  blogObject = new Blog(initialBlogs[1])
+  blogObject = new Blog(helper.initialBlogs[1])
   await blogObject.save()
 })
 
 test('number of blogs', async () => {
   const response = await api.get('/api/blogs')
-  expect(response.body).toHaveLength(initialBlogs.length)
+  expect(response.body).toHaveLength(helper.initialBlogs.length)
 })
 
 // // Tehtävä 4.9
 test('blog id', async () => {
   const response = await api.get('/api/blogs')
   const respObj = response.body
-  console.log(response.body.map(body => body.id));
   expect(response.body.map(body => body.id)[0]).toBeDefined()
 })
 
@@ -58,14 +40,21 @@ test('add new blog', async () => {
     .expect(201)
     .expect('Content-Type', /application\/json/)
 
-  const response = await api.get('/api/blogs')
-  expect(response.body).toHaveLength(initialBlogs.length+1)
+  
+  const blogs = await helper.blogsInDb() // Get blogs
+  expect(blogs).toHaveLength(helper.initialBlogs.length+1)
 
-  const titles = response.body.map(res => res.title)
+  const titles = blogs.map(res => res.title)
 
   expect(titles).toContain(
     'UusiBlogi'
     )
+})
+
+//Tee testi, joka varmistaa, että jos kentälle likes ei anneta arvoa, asetetaan sen arvoksi 0. Muiden kenttien sisällöstä ei tässä tehtävässä vielä välitetä.
+// 4.11
+test('add non liked blog', async () => {
+
 
 })
 
